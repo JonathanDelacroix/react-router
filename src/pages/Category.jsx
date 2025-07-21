2229597422f1fd72a761cc20c7e6869f64b065b7
delacroix <delacroix.jon@gmail.com>
2025-07-21 16:48:23 +0200

    Activité 8

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,10 +1,20 @@
-import { useParams } from 'react-router';
+import { useParams, useNavigate } from 'react-router';
+import { Button } from 'react-bootstrap';
 
 const Category = () => {
-const { category } = useParams();
+    const { category } = useParams();
+    const navigate = useNavigate();
+    
+    const handleBackToHome = () => {
+        navigate('/');
+    };
+    
     return (
         <div>
             <h1>Catégorie : {category}</h1>
+            <Button variant="outline-danger" onClick={handleBackToHome} className="mb-3">
+                Retour
+            </Button>
         </div>
     );
 };

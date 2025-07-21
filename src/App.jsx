2320941ea8185fc7cd6a812f4b0cfdf1772aafc3
delacroix <delacroix.jon@gmail.com>
2025-07-21 11:00:09 +0200

    Activité 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router'
 import Home from './pages/Home.jsx'
 import './App.css'
+import Layout from './pages/components/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout/>}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   )
 }

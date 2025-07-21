896db9728152a72a3706fb24379513c8a0c14a71
delacroix <delacroix.jon@gmail.com>
2025-07-21 16:36:42 +0200

    Activité 7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,28 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { NavLink } from 'react-router';
+import { NavLink, useMatch } from 'react-router';
 import '../assets/Header.css';
 
 function Header() {
+  const match = useMatch('/categorie/:category');
+  
   return (
     <header>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Collapse id="navbar-nav" className="justify-content-end">
-            <Nav>
-              <Nav.Link as={NavLink} to="/" >Accueil</Nav.Link>
-              <Nav.Link as={NavLink} to="/categorie/vetements" >Vêtements</Nav.Link>
-              <Nav.Link as={NavLink} to="/categorie/chaussures" >Chaussures</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+            <div>
+              {match ? (
+                <span>
+                  Catégorie : {match.params.category}
+                </span>
+              ) : null}
+            </div>
+            <Navbar.Collapse id="navbar-nav" className="justify-content-end">
+              <Nav>
+                <Nav.Link as={NavLink} to="/">Accueil</Nav.Link>
+                <Nav.Link as={NavLink} to="/categorie/vetements">Vêtements</Nav.Link>
+                <Nav.Link as={NavLink} to="/categorie/chaussures">Chaussures</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
